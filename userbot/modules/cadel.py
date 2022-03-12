cde0fd09577af3d0561291ae7ea249e2957a0e32
fatur <90284737+DIORrios285@users.noreply.github.com>
2022-03-12 14:29:26 +0800

    Create cadel.py

---
+++ userbot/modules/cadel.py
@@ -0,0 +1,38 @@
+import random
+from userbot import owner
+from telethon.tl.types import InputMessagesFilterVideo
+from telethon.tl.types import InputMessagesFilterVoice
+from userbot import CMD_HELP
+from userbot import CMD_HANDLER as cmd
+from userbot.utils import fanda_cmd
+
+
+@fanda_cmd(pattern="cadel$")
+async def _(event):
+    try:
+        rrrr = [
+            rrrrrr
+            async for asupan in event.client.iter_messages(
+                "@sicadelini", filter=InputMessagesFilterVoice
+            )
+        ]
+        await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(rrrr),
+            caption=f"**Nih banh**.")
+        
+        await event.delete()
+    except Exception:
+        await event.edit("Tidak bisa menemukan voice msg.")
+
+        
+
+CMD_HELP.update(
+    {
+        "asupan": f"**Plugin : **`asupan`\
+        \n\n  •  **Syntax :** `{cmd}asupan`\
+        \n  •  **Function : **Untuk Mengirim video asupan secara random.\
+    "
+    }
+)
