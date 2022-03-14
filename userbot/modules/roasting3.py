3e7c438299e953690b8498aafda5f857df111d30
fatur <90284737+DIORrios285@users.noreply.github.com>
2022-03-15 02:33:50 +0800

    Create roasting3.py

---
+++ userbot/modules/roasting3.py
@@ -0,0 +1,165 @@
+# kumpulin kosakata makanya bgst
+# mks smsm
+
+from time import sleep
+from userbot import CMD_HELP
+from userbot.events import register
+
+# PANTUNIN DULU SI JAMET
+
+@register(outgoing=True, pattern='^.pt(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**beli vakum dibalikpapan assalamualaikum biar sopan**")
+# dior cakep
+
+@register(outgoing=True, pattern='^.ptn(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**KETEMU SI MAMAS DIAJAKIN KE CIBINONG PULANG NYE DIANTERIN MAKE KOPAJA EH BOCAH AMPAS MUKA HINA KEK ODONG ODONG GA USAH SO KERAS KALO MENTAL MASIH BLOM SEKERAS BAJA.**")
+# dior cakep!
+
+@register(outgoing=True, pattern='^.pn(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**DUA TIGA TUTUP BOTOL, MUKA LO KAYAK KONTOL**")
+# dior cakep!
+
+@register(outgoing=True, pattern='^.pan(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**KEPASAR BELI INDUK PULANG NYA LUPA BELI TAPE SAMA KAPUK EH KACUNG GAUSH SO GANTENG MUKA LO BULUK UDH KAYA TAPE BUSUK**")
+# dior cakep!
+
+
+
+# War anjing
+
+@register(outgoing=True, pattern='^.ros(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MUKA LU JELEK BEGITU TOLOL MUKA KAYAK TAPE BASI BELAGU LU KONTOK GUA KASIH TAU AJA BUAT LU BOCAH TOLOL BOCAH BOCAH KAYAK LU NI YANG SUKA MAIN DISAWAG NGEJAR NGEJAR LAYANGAN BEGITU SAMBIL MAIN LUMPUR LUMPURAN SAMPE MUKA LU ITEM BEGITU KAYAK ABIS MAGHRIB APALAGI LU MANDI MASIH DIKALI BEGITU SAMBIL NYUCI BAJU BARENG EMAK LU DIJEMUR DIATAS RUMAH LU YANG KECIL BEGITU TOLOK KARNA KELUARGA LU KELUARGA PALING MISKIN DALAM SEGALA HAL TOLOL KRNA KELUARGA LU SEMUANYA GAPUNYA PENDIDIKAN PANTESAN SUKA DIBEGO BEGOIN SAMA ORANG TOLOL APALAGI NI YAA KALO LAGI HUJAN ITU KESEMPATAN LU BUAT NGELUARIN EMBER BUAT NGAMBIL AIR HUJAN KARNA DIRUMAH LU GADA AIR LAGI BUAT MANDI TOLOL SABUN AJA UDH BOLONG BEGITU DIPAKEIN BAPALU COLI SAMPE JEMBUT NYA RONTOK BEGITU UDH KAYAK RAMBUT AJA TAPI KAN BAPALU BOTAK BEGITU YA GAPUNYA RAMBUT SAMA AHA OTAKNYA JUGA KOPONG KAYAK DENGKULNYA BAPALU UDH PUNYA ISTRI MSIH AJA DOYAN COLI TOLOL MUNGKIN KARNA MEMEK MAK LU UDH JEBOL GEDE BEGITU DITUSBOLIN PAKE PISO DAPUR YANG TAJEM BEGITU KAYAK KUKU BAPALU GADIPOTONG POTONG SAMPE TAJEM BEGITU UDH KAYAK JAMET PUNYA KUKU PANJANG PANJANG BIAR DIKATA JAGO KALI YA PADAHAL KAYAK ORANG IDIOT KUKU DIPANJANGIN KONTOL DIPANJANGIN BIAR PADA DOYAN TOLOL BILANGIN KEBAPALU LU TUH DONGO LU KALO JADI ANAK YANG BENER YANG NGELAWAN ORANG TUA TOLOL TIAP HARI BUKA TELE DISURUH BELI KECAP NOLAK MULU LU TOLOL ANAK GGUNA GATAU TERIMAKASIH DARI ORANG TUA LU TOLOL BIAR DIKATA KELUARGA LU KELUARGA MISKIN PENDIDIKAN MISKIN HARTA MISKIN SEMUANYA LU HARUS BERTERIMAKASIH SAMA BAPALU LU YANG GAGUNA ITU KONTOL BIAR KEDEPANNYA LU GA SESAT APALAGI LU HARUS BERTERIMAKASIH SAMA GUA KARNA BAPALU AJA MASIH DIBIAYAIN SAMA GUA BEGO Nyerocos LU MASIH Lama  GE TOLOL**")
+# dior cakep!
+
+@register(outgoing=True, pattern='^.wr(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**EH KONTOL GUA KASIH TAU NIH YAA, BOCAH BOCAH KAYA LU MAH KAGA PANTES PISAN MAIN DI TELE, KARNA APA? JARI JARI TANGAN LU AJA ITU PADA BUNTUNG BEGO, BIBIR LU JUGA SUMBING BEGITU, GUA TAU NIH YAA PAS MASIH DI DALEM KANDUNGAN, LU TUH GAK BISA DIEM KAN, MANGKANYA LU JADI BEGINI, CACAT BET CACAT, GUA MAH KASIAN SAMA LU, PAS MASIH BAYI JUGA LU KAGA DI KASIH AIR ASI KAN, EMANG LU PANTES DAPET BEGITU, DI KASIH AIR COMBERAN JADI OTAKNYA DONGO BEGINI NIH, LAGIAN JUGA YAA MAUPUN LU DI LAHIRIN DENGAN SEMPURNA LU JUGA GAK GUNA, KARNA APA, LU EMANG UDAH GAK PANTES TINGGAL DI BUMI ANJINGGG, LU TUH PANTESNYA TINGGAL DI DALEM BUMI GITU TUH, SENDIRIAN GAK PUNYA TEMEN, JADI ORANG GAK BERGUNA, LAGIAN JUGA KALO LU TINGGAL DI BUMI, LU TUH GAK PANTES DAN GAK COCOK, KARNA APA, KARNA LU TUH SEMUA SERBA KEKURANGAN, LU MAU TINGGAL DI LUAR ANGKASA JUGA GUA YAKIN ALIEN KAGA MAU TERIMA LU, KARNA LU TUH GAK SESUAI KRETERIA MEREKA, LU TUH JELEK BET JELEK, LEBIH JELEK DARI PADA MUKA KECOA, BISA SIH LU TINGGAL DI LUAR ANGKASA, ITU JUGA PALING JAUH, JAUH DARI PERADABAN HAHAHA, KARNA TEMPAT TEMPAT BEGITU EMANG COCOK BUAT LU, KALO DI PIKIR PIKIR OTAK SEMUT SAMA OTAK LU JUGA GUA RASA PINTERAN OTAK SEMUT GITU, KARNA APA, LU TUH DONGO NYA JELAS BANGET, SAMPE KE TULANG TULANG, KE BOOL BOOL, KE SEL SEL DARAH LU IKUTAN BLOON HAHAHAHA**")
+# dior cakep!
+
+@register(outgoing=True, pattern='^.ro(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**EH ANJING GW KASIH LIAT KOSA KATA YANG BERDAMAGE**")
+    sleep(2)
+    await typew.edit("**LIAT KOSA KATA LO YANG UDAH BASI TOLOL YANG BANYAK DI PAKE ORANG SAMA KAYA EMAK LO YANG SERING DI PAKAI ORANG TOLOL, MAK NYA UDAH KAYA ROKOK SATU BATANG BISA DI PAKE RAME RAME YAKAN ITU MAK LO TOLOL KASIAN DIA JAM SEGINI BELOM PULANG KARENA MASI CARI UANG PAKAI MEMEK NYA YANG UDAH BECEK ITU TOLOL APA LO KAGA KASIAN BEGO LIAT MAK LO YANG KEK GITU MENDINH MAMAK LO SURUH KERJA YANG BENER TOLOL JANGAN NGELONTE MULU, KASIAN JUGA LIAT BAPAK LO BAU TANAH YHAHAHAHA**")
+# dior cakep!
+
+@register(outgoing=True, pattern='^.rst(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**⁪⁬EHH DASARR BOCAH GELAS PECAH KEPALA KOTAK KAYAK SEMPAK YANG DI BUANG KESAMPAH**")
+    sleep(4)
+    await typew.edit("**DASAR KAU SETAN ALAS MUKA KAYAK TERAS BANTAL BUSUK OTAK SEMPIT KAYAK LUBANG ANUS KUTENDANG KAU LANGSUNG PINCANG KU GAMPAR KAU KESURUPAN KUCIDORII KAU LANGSUNG KEJANG KEJANG KUHANTAM MUKA KAU BONYOK KAYAK PEMANJANG JEMBUT WAK DOYOK**")
+    sleep(4)
+    await typew.edit("**EHH DASAR KELUARGA KERTAS MATA BESAR KAYAK MATA CYCLOPS KU SENIPER KEPALA KAU HANCUR MUKA ROBEX KAYAK SOFTEX AYU TING TING KU SLEDING MALAH BUNTING TOLOL JANCOK**")
+    sleep(4)
+    await typew.edit("**BOCAH SUPERMEN MERANGKAK KULIT HIJAU KAYAK HULUK KURUS KERING NGOCOK PAKAI MESIN CUCI KU TONJOK MALAH BERUBAH JADI POWER RENJER**")
+# dior cakep!
+
+
+@register(outgoing=True, pattern="^.title(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**OI ANAK TITLE**")
+    sleep(2)
+    await typew.edit("**OOO INI YANG SOK JADI PAHLAWAN DI TELEGRAM?**")
+    sleep(3)
+    await typew.edit("**TITLE KEMANA MANA SAMPE MENUHIN NAMA**")
+    sleep(2)
+    await typew.edit("**ADA YANG SAMPE 18+ LAH SEGALA MACEM**")
+    sleep(2)
+    await typew.edit("**LO KIRA KEREN KEK GITU?**")
+    sleep(2)
+    await typew.edit("**KERJAAN CUMA NGURUSIN GRUP DI TELEGRAM SAMA NGAJAK ORANG WAR**")
+    sleep(4)
+    await typew.edit("**YAELAH BRO MENTAL LO CUMA DI SOSMED APA GIMANE?**")
+    sleep(2)
+    await typew.edit(
+        "**PERASAAN DULU TELEGRAM GAADA DEH BOCAH BOCAH SOK JAGO KEK GINI**"
+    )
+    sleep(2)
+    await typew.edit("**GILIRAN TITLE NYA DI EJEK NGADU KE OWNER NYA**")
+    sleep(4)
+    await typew.edit("**TRUS NGAJAK WAR**")
+    sleep(2)
+    await typew.edit("**BUSET DAH BANG**")
+    sleep(2)
+    await typew.edit("**UDAH SEJAGO APESI SAMPE GC DIBELA BELA**")
+    sleep(3)
+    await typew.edit("**ORANG TUA LO NOH ADA YANG NAGIH UTANG UDA LO BELA BELOM?**")
+    sleep(4)
+    await typew.edit("**RELA NGUTANG DEMI NGIDUPIN LU**")
+    sleep(2)
+    await typew.edit("**EH ANAKNYA MALAH NGEBELAIN GC GAJELAS HAHAHA**")
+    sleep(3)
+    await typew.edit("**MANA VIRTUAL LAGI, SOK JAGO LAGI DUH**")
+    sleep(3)
+    await typew.edit("**SEMOGA CEPET SADAR YA HAHAHAHA**")
+
+
+@register(outgoing=True, pattern="^.alay(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Halo kak**")
+    sleep(1)
+    await typew.edit("**Gua liat-liat lu main bot mulu**")
+    sleep(2)
+    await typew.edit("**Alay banget sumpah**")
+    sleep(2)
+    await typew.edit("**Baru pasang ucelbot ya?**")
+    sleep(2)
+    await typew.edit("**Pantesan norak yahaha**")
+    sleep(2)
+    await typew.edit("**Kalo mau coba coba command di gc pribadi aja**")
+    sleep(2)
+    await typew.edit("**Jangan di publik, jijik liatnya anjg:v**")
+    sleep(2)
+    await typew.edit("**Intinya lo alay maen bot mulu**")
+    sleep(2)
+    await typew.edit("**Lawriiiiiiieeeee:v**")
+
+
+CMD_HELP.update({
+    "pantun":
+    ".pt\
+\nUsage: Pantun berdamage mks smsm.\
+    \n\n.ptn\
+\nUsage: Pantun berdamage mks smsm.\
+    \n\n.pn\
+\nUsage: Pantun berdamage mks smsm.\
+    \n\n.pan\
+\nUsage: Pantun berdamage mks smsm."
+})
+        
+CMD_HELP.update({
+    "roasting":
+    ".ros\
+\nUsage: War ngentot mks smsm.\
+    \n\n.wr\
+\nUsage: War ngentot mks smsm.\
+    \n\n.ro\
+\nUsage: War ngentot mks smsm.\
+    \n\n.title\
+\nUsage: Hina si maruk title taik anjing.\
+    \n\n.ro\
+\nUsage: Hina manusia norak/alay.\
+    \n\n.alay\
+\nUsage: War ngentot mks smsm."
+})
