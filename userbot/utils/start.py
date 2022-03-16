0630ec0b5d55c5448fc9320e7d41b291261e3c49
fatur <90284737+DIORrios285@users.noreply.github.com>
2022-03-16 16:13:10 +0800

    Update start.py

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -11,8 +11,8 @@ async def startupmessage():
         if BOTLOG:
             await tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/02f87cca391f9b9d627d5.jpg",
-                caption="**Fanda Userbot Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @Fandaproject ",
+                "https://telegra.ph/file/8ee507ef2468557fc49e0.jpg",
+                caption="**Fanda-Userbot started**!\n\n**Python:** `3.9.10`\n**Pyrogram:** `1.4.8`\n**PyTgCalls:** `0.9.0 Beta 1`",
                 buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/Fandasupport"),)],
             )
     except Exception as e:
