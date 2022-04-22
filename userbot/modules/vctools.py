d265295b1b529b1ce31786ae4bbbe53023c9b851
fatur <90284737+DIORrios285@users.noreply.github.com>
2022-04-22 15:57:37 +0800

    Update vctools.py

--- userbot/modules/vctools.py
+++ userbot/modules/vctools.py
@@ -125,7 +125,7 @@ async def _(event):
     else:
         chat_id = event.chat_id
     if chat_id:
-        file = "./userbot/resources/audio-ini.mp3"
+        file = "./resources/audio-ini.mp3"
         try:
             await call_py.join_group_call(
                 chat_id,
